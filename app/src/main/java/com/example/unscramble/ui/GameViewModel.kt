ca6bf68560645ef1f9eb473268895a548ccec292
Nick17123 <mahardiannicholas@gmail.com>
2026-04-16 01:56:30 +0700

    Membuat class bernama GameViewModel didalam package ui. dengan ekstensi fungsi ViewModel

---
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.unscramble.ui
+
+import androidx.lifecycle.ViewModel
+
+// membuat class GameViewMode didalam package Ui dengan ekstensi compose.
+class GameViewModel: ViewModel() {
+
+}
