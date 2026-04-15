9a566990ef6d1c4eea7ab6a442da8615518a4438
Nick17123 <mahardiannicholas@gmail.com>
2026-04-16 03:12:28 +0700

    // membuat variabel uiState bertipe StateFlow<GameUiState>

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -2,9 +2,14 @@ package com.example.unscramble.ui
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 // membuat class GameViewMode didalam package Ui dengan ekstensi compose.
 class GameViewModel: ViewModel() {
-    //menambahkan variabel uiState dengan tipe MutableStateFlow<GameUiState>
+    // menambahkan variabel uiState dengan tipe MutableStateFlow<GameUiState>
+    // merupakan backing property umtuk menghindari update dari classes lain
     private val _uiState = MutableStateFlow(GameUiState())
+    // membuat variabel uiState dengan tipe StateFlow<GameUiState>
+    val uiState : StateFlow<GameUiState>
+
 }
