b633e8cc71e963993f60b4675830ab9ba8a83ac4
Nick17123 <mahardiannicholas@gmail.com>
2026-04-16 02:22:54 +0700

    membuat data class GameUiState didalam package Ui dan variabel currentScrambledWord.

---
+++ app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -0,0 +1,6 @@
+package com.example.unscramble.ui
+
+// membuat data class GameUiState didalam package Ui dan variabel currentScrambledWord.
+data class GameUiState(
+    val currentScrambledWord: String = ""
+)
