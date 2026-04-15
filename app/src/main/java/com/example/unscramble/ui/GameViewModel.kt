71445ead1adda3851e3e5b3f1c2e837580c120d2
Nick17123 <mahardiannicholas@gmail.com>
2026-04-16 04:44:22 +0700

    menambahkan methode shuffleCurrentWord() untuk mengacak kata yang diberikan dan mengembalikan kata yang telah diacak sebagai string dan Menambahkan fungsi helper yang bernama resetGame() untuk menginisialisasi permainan nantinya digunakan untuk start dan restart permainan.

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -19,6 +19,9 @@ class GameViewModel: ViewModel() {
     // menambahkan variabel currentWord dengan tipe String untuk menyimpan kata saat ini.
     private lateinit var currentWord: String
 
+    // menambahkan variabel usedWords dengan tipe MutableSet<String> untuk menyimpan daftar kata yang sudah digunakan.
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
     // menambahkan methode pickRandomWordAndShuffle() untuk memilih kata secara acak dari daftar kata yang ada
     // dalam variabel allWords. Kemudian mengacak kata tersebut menggunakan fungsi shuffleCurrentWord().
     private fun pickRandomWordAndShuffle(): String {
@@ -30,6 +33,22 @@ class GameViewModel: ViewModel() {
             usedWords.add(currentWord)
             return shuffleCurrentWord(currentWord)
         }
+    }
+    // menambahkan methode shuffleCurrentWord() untuk mengacak kata yang diberikan
+    // dan mengembalikan kata yang telah diacak sebagai string.
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        //  mengacak kata
+        tempWord.shuffle()
+        while (String(tempWord).equals(word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+    // menambahkan fungsi helper untuk start dan restart permainan
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
     }
 
 }
